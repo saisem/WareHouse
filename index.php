07e43360749e7c4099a0ee478759fe508b73acff
Erwin Laura Cuisara <watafackerwin2@gmail.com>
2019-12-19 23:37:15 -0400

    ssadsa

--- index.php
+++ index.php
@@ -186,7 +186,7 @@
                     <div class="col-lg-12">
                         <div class="card">
                             <div class="card-body">
-                                <h4 class="box-title">Traffic </h4>
+                                <h4 class="box-title">HOSPEDAJES</h4>
                             </div>
                             <div class="row">
                                 <div class="col-lg-8">
@@ -198,14 +198,14 @@
                                 <div class="col-lg-4">
                                     <div class="card-body">
                                         <div class="progress-box progress-1">
-                                            <h4 class="por-title">Visits</h4>
+                                            <h4 class="por-title">TODOS LOS AÑOS</h4>
                                             <div class="por-txt">Santa Cruz <?php echo total("'Santa Cruz'");?></div>
                                             <div class="progress mb-2" style="height: 5px;">
-                                                <div class="progress-bar bg-flat-color-1" role="progressbar" style="width: 40%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div class="progress-bar bg-flat-color-1" role="progressbar" style="width: 90%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
                                         </div>
                                         <div class="progress-box progress-2">
-                                            <h4 class="por-title">Bounce Rate</h4>
+                                            <h4 class="por-title"></h4>
                                             <div class="por-txt">Cochabama <?php echo total("'Cochabamba'");?></div>
                                             <div class="progress mb-2" style="height: 5px;">
                                                 <div class="progress-bar bg-flat-color-2" role="progressbar" style="width: 24%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
@@ -213,10 +213,10 @@
                                         </div>
                                        
                                         <div class="progress-box progress-2">
-                                            <h4 class="por-title">Targeted  Visitors</h4>
+                                            <h4 class="por-title"></h4>
                                             <div class="por-txt">La Paz <?php echo total("'La Paz'");?></div>
                                             <div class="progress mb-2" style="height: 5px;">
-                                                <div class="progress-bar bg-flat-color-4" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                                <div class="progress-bar bg-flat-color-4" role="progressbar" style="width: 60%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                                             </div>
                                         </div>
                                     </div> <!-- /.card-body -->
